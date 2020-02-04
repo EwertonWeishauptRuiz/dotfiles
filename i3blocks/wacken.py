6d97ed26dddb357fff96b337c2f325678678136d
Ewerton Weishaupt Ruiz <ewr@intempus.dk>
2020-02-04 10:28:05 +0100

    Add new Dates for Festivals to i3blocks

--- i3blocks/wacken.py
+++ i3blocks/wacken.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 
 from datetime import date
 
 wacken_date = date(2020, 07, 30)
-remaining = wacken_date - date.today()
-print remaining.days
+copenhell_date = date(2020, 06, 18)
+ppeurope_date = date(2020, 10, 2)
+remaining_wacken = wacken_date - date.today()
+remaining_copenhell = copenhell_date - date.today()
+remaining_ppeurope = ppeurope_date - date.today()
+print 'ꟼP ', remaining_ppeurope.days, " | W:O:A ", remaining_wacken.days, ' | ₦ ', remaining_copenhell.days
+
