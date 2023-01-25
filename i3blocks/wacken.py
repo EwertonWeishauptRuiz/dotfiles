a60498cf560c4f48019d2173ac63346537f18ef7
shakamaiden <shakamaiden@gmail.com>
2023-01-25 14:00:24 +0100

    Updated festival i3block

--- i3blocks/wacken.py
+++ i3blocks/wacken.py
@@ -4,11 +4,9 @@
 
 from datetime import date
 
-wacken_date = date(2020, 07, 30)
-copenhell_date = date(2020, 06, 18)
-ppeurope_date = date(2020, 10, 2)
+wacken_date = date(2023, 8, 2)
+copenhell_date = date(2023, 6, 18)
 remaining_wacken = wacken_date - date.today()
 remaining_copenhell = copenhell_date - date.today()
-remaining_ppeurope = ppeurope_date - date.today()
-print 'ꟼP ', remaining_ppeurope.days, " | W:O:A ", remaining_wacken.days, ' | ₦ ', remaining_copenhell.days
+print(f"W:O:A {remaining_wacken.days}  |  ₦ {remaining_copenhell.days}")
 
