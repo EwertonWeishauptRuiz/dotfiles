8ee2cfede762b1e1dda5064f98b2adef3d41576d
Ewerton Ruiz <ewerton.ruiz@visma.com>
2025-05-22 14:52:43 +0200

    Update battery script to properlly handle 100%

--- i3blocks/battery.sh
+++ i3blocks/battery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
+BAT=$(acpi -b | grep -E -o '[0-9]{1,3}%')
 
 # Full and short texts
 echo "$BAT"
@@ -9,5 +9,8 @@ echo "$BAT"
 # Set urgent flag below 5% or use orange below 20%
 [ ${BAT%?} -le 5 ] && exit 33
 [ ${BAT%?} -le 20 ] && echo "#FF8000"
+
+# Set green if 100%
+[ ${BAT%?} -eq 100 ] && echo "#00FF00"
 
 exit 0
